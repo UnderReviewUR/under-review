8af2a757f81debb7c1a06023837b61094aea5d6b
UnderReviewUR <jon.shepherd@myyahoo.com>
2026-03-16 12:37:48 -0500

    Create model-props.js

---
+++ api/model-props.js
@@ -0,0 +1,15 @@
+export default async function handler(req, res) {
+  try {
+    const props = [
+      { player: "Jayson Tatum", team: "BOS · SF", line: "28.5", stat: "PTS", dir: "over" },
+      { player: "Nikola Jokic", team: "DEN · C", line: "27.5", stat: "PTS", dir: "over" },
+      { player: "Jalen Brunson", team: "NYK · PG", line: "27.5", stat: "PTS", dir: "over" },
+      { player: "LeBron James", team: "LAL · SF", line: "24.5", stat: "PTS", dir: "over" },
+      { player: "Bam Adebayo", team: "MIA · C", line: "18.5", stat: "PTS", dir: "over" }
+    ];
+
+    return res.status(200).json(props);
+  } catch (err) {
+    return res.status(500).json({ error: "Failed to build model props." });
+  }
+}
