c7679f46b22cd4daafe10b03cccfe58523a493cf
UnderReviewUR <jon.shepherd@myyahoo.com>
2026-03-16 10:46:10 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://under-review.vercel.app',
+        changeOrigin: true,
+      }
+    }
+  }
 })
